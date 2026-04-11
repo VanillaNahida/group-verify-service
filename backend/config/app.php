33f017af69695e5793f3e5463a54f2b1920d1333
zhaoyj <admin@silveridc.cn>
2026-04-12 01:43:25 +0800

    Update app.php

--- backend/config/app.php
+++ backend/config/app.php
@@ -2,29 +2,29 @@
 // +----------------------------------------------------------------------
 // | 应用设置
 // +----------------------------------------------------------------------
-
 return [
+    // 应用地址
+    'app_host'         => env('app.host', ''),
     // 应用的命名空间
-    'app_namespace'    => env('APP_NAMESPACE', ''),
+    'app_namespace'    => '',
     // 是否启用路由
-    'with_route'       => env('WITH_ROUTE', true),
+    'with_route'       => true,
+    // 应用快速访问
+    'app_express'      => true,
     // 默认应用
-    'default_app'      => env('DEFAULT_APP', 'index'),
+    'default_app'      => 'home',
     // 默认时区
-    'default_timezone' => env('DEFAULT_TIMEZONE', 'Asia/Shanghai'),
-
+    'default_timezone' => 'Asia/Shanghai',
     // 应用映射（自动多应用模式有效）
-    'app_map'          => [],
-    // 域名绑定（自动多应用模式有效）
+    'app_map' => [],
+    // 域名绑定
     'domain_bind'      => [],
-    // 禁止URL访问的应用列表（自动多应用模式有效）
+    // 禁止URL访问的应用列表
     'deny_app_list'    => [],
-
     // 异常页面的模板文件
-    'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
-
-    // 错误显示信息,非调试模式有效
-    'error_message'    => env('ERROR_MESSAGE', '页面错误！请稍后再试～'),
+    'exception_tmpl' => app()->getThinkPath() . 'tpl/think_exception.tpl',
+    // 错误显示信息
+    'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => env('SHOW_ERROR_MSG', false),
+    'show_error_msg'   => false,
 ];
